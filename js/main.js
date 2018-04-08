ef002e31caac1d4f612f69e9253f580c748a2833
Alexis Berry <alexisberry@alexiss-mbp-2.attlocal.net>
2018-04-07 21:12:21 -0400

    add contact info, social icons and map plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
     $.backstretch("img/apron.jpg");
     $( '#calendar' ).calendario();
+    new Maplace({
+    locations: data,
+    controls_type: 'list',
+    controls_on_map: false
+}).Load();
 
 });
+var data = [{
+        lat: 25.8,
+        lon: -80.19,
+        title: 'Nancy',
+        html: '<h3>Nancy</h3>',
+        zoom: 8,
+        icon: 'http://www.google.com/mapfiles/markerA.png'
+    }
+];
